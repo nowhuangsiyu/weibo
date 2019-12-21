8403d19a8b4639934ca69a71f933c76e7b970be3
474174312 <474174312@qq.com>
2019-12-21 06:49:59 +0000

    完成用户注册功能

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -7,24 +7,36 @@ use Illuminate\Http\Request;
 
 class UsersController extends Controller
 {
-    //
+    //新建用户
     public function create()
     {
         return view('users/create');
     }
 
+    //显示用户
     public function show(User $user)
     {
         return view('users/show', compact('user'));
     }
 
+    //注册用户
     public function store(Request $request)
     {
+        //进行信息过滤操作
         $this->validate($request, [
             'name' => 'required|max:50',
             'email' => 'required|email|unique:users|max:255',
             'password' => 'required|confirmed|min:6'
         ]);
-        return;
+        //进行注册操作
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => bcrypt($request->password),
+        ]);
+        //添加注册成功的信息提示
+        session()->flash('success', '欢迎，您将在这里开启一段新的旅程~');
+        //返回注册信息
+        return redirect()->route('users.show', [$user]);
     }
 }
