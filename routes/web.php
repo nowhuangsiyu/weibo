6d89abcbc4806c4623c07629d70dd3d8d0786bbc
474174312 <474174312@qq.com>
2019-12-21 07:59:57 +0000

    创建会话

--- routes/web.php
+++ routes/web.php
@@ -19,5 +19,11 @@ Route::get('/', 'StaticPagesController@home')->name('home');
 Route::get('/help', 'StaticPagesController@help')->name('help');
 Route::get('/about', 'StaticPagesController@about')->name('about');
 
+//用户注册
 Route::get('/signup', 'UsersController@create')->name('signup');
 Route::resource('users', 'UsersController');
+
+//用户登录退出
+Route::get('login', 'SessionsController@create')->name('login');
+Route::post('login', 'SessionsController@store')->name('login');
+Route::delete('logout', 'SessionsController@destroy')->name('logout');
