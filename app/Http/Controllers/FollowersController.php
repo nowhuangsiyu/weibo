59cb6e8054d51dbc1bd8c299745ca48cf08e1729
474174312 <474174312@qq.com>
2020-01-25 12:04:44 +0000

    关注用户动态流

---
+++ app/Http/Controllers/FollowersController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+use Auth;
+
+class FollowersController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    public function store(User $user)
+    {
+        $this->authorize('follow', $user);
+
+        if(!Auth::user()->isFollowing($user->id)) {
+            Auth::user()->follow($user->id);
+        }
+
+        return redirect()->route('users.show', $user->id);
+    }
+
+    public function destroy(User $user)
+    {
+        $this->authorize('follow', $user);
+
+        if(Auth::user()->isFollowing($user->id)) {
+            Auth::user()->unfollow($user->id);
+        }
+
+        return redirect()->route('users.show', $user->id);
+    }
+
+    public function follow(User $currentUser, User $user)
+    {
+        return $currentUser->id !== $user->id;
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -43,3 +43,7 @@ Route::resource('statuses', 'StatusesController', ['only' => ['store', 'destroy'
 //用户关注的人/粉丝列表
 Route::get('/users/{user}/followings', 'UsersController@followings')->name('users.followings');
 Route::get('/users/{user}/followers', 'UsersController@followers')->name('users.followers');
+
+//关注用户/取消关注
+Route::post('/users/followers/{user}', 'FollowersController@store')->name('followers.store');
+Route::delete('/users/followers/{user}', 'FollowersController@destroy')->name('followers.destroy');
